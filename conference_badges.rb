0d986455a2b2476c81604cafafff8d7cb6fd7cdc
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-15 12:26:15 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,11 @@ def badge_maker(name)
 "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(attendees)
+    bagdes
+    
+
 def assign_rooms(number)
     "Hello, _____! You'll be assigned to room _____!"
+end
 end
