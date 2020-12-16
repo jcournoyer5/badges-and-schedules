3c8e307413c814990a3a6a772e5e75c306fd29f1
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-16 01:42:05 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,13 +12,14 @@ end
 def assign_rooms(attendees)
     attendees.map.with_index(1) do |person, room_num|
     "Hello, #{person}! You'll be assigned to room #{room_num}!"
-end
+    end
 end
 
 def printer(attendees)
     batch_badge_creator(attendees).each do |badge|
         puts badge
     end
+    
     assign_rooms(attendees).each do |room|
         puts room
 end
